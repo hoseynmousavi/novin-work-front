3f8a3f45df0ffc331460fba4e4965dd564e263c9
Hoseyn <hoseyn.mousavi78@gmail.com>
2021-06-08 15:17:30 +0430

    fx

---
+++ src/constant/ResumeList.js
@@ -0,0 +1,131 @@
+import resumeThumb from "../media/images/resume/resume-thumb.jpg"
+import resumeMain from "../media/images/resume/resume-main.jpg"
+import resumeThumb1 from "../media/images/resume/resume-thumb1.jpg"
+import resumeMain1 from "../media/images/resume/resume-main1.jpg"
+import resumeThumb2 from "../media/images/resume/resume-thumb2.jpg"
+import resumeMain2 from "../media/images/resume/resume-main2.jpg"
+import resumeThumb3 from "../media/images/resume/resume-thumb3.jpg"
+import resumeMain3 from "../media/images/resume/resume-main3.jpg"
+import resumeThumb4 from "../media/images/resume/resume-thumb4.jpg"
+import resumeMain4 from "../media/images/resume/resume-main4.jpg"
+import resumeThumb5 from "../media/images/resume/resume-thumb5.jpg"
+import resumeMain5 from "../media/images/resume/resume-main5.jpg"
+import resumeThumb7 from "../media/images/resume/resume-thumb7.jpg"
+import resumeMain7 from "../media/images/resume/resume-main7.jpg"
+import resumeThumb8 from "../media/images/resume/resume-thumb8.jpg"
+import resumeMain8 from "../media/images/resume/resume-main8.jpg"
+import resumeThumb9 from "../media/images/resume/resume-thumb9.jpg"
+import resumeMain9 from "../media/images/resume/resume-main9.jpg"
+import resumeThumb11 from "../media/images/resume/resume-thumb11.jpg"
+import resumeMain11 from "../media/images/resume/resume-main11.jpg"
+import resumeThumb12 from "../media/images/resume/resume-thumb12.jpg"
+import resumeMain12 from "../media/images/resume/resume-main12.jpg"
+import resumeThumb13 from "../media/images/resume/resume-thumb13.jpg"
+import resumeMain13 from "../media/images/resume/resume-main13.jpg"
+import resumeThumb14 from "../media/images/resume/resume-thumb14.jpg"
+import resumeMain14 from "../media/images/resume/resume-main14.jpg"
+import resumeThumb15 from "../media/images/resume/resume-thumb15.jpg"
+import resumeMain15 from "../media/images/resume/resume-main15.jpg"
+import resumeThumb16 from "../media/images/resume/resume-thumb16.jpg"
+import resumeMain16 from "../media/images/resume/resume-main16.jpg"
+import resumeThumb17 from "../media/images/resume/resume-thumb17.jpg"
+import resumeMain17 from "../media/images/resume/resume-main17.jpg"
+import resumeThumb18 from "../media/images/resume/resume-thumb18.jpg"
+import resumeMain18 from "../media/images/resume/resume-main18.jpg"
+import resumeThumb19 from "../media/images/resume/resume-thumb19.jpg"
+import resumeMain19 from "../media/images/resume/resume-main19.jpg"
+
+const ResumeList = [
+    {
+        name: "طراحی سایت فروشگاهی لوازم دیجیتال",
+        thumb: resumeThumb,
+        main: resumeMain,
+    },
+    {
+        name: "طراحی سایت آموزش ورزش",
+        thumb: resumeThumb1,
+        main: resumeMain1,
+    },
+    {
+        name: "طراحی سایت فروشگاهی",
+        thumb: resumeThumb2,
+        main: resumeMain2,
+    },
+    {
+        name: "طراحی سایت مشاوره آنلاین",
+        thumb: resumeThumb3,
+        main: resumeMain3,
+    },
+    {
+        name: "طراحی سایت خبری",
+        thumb: resumeThumb4,
+        main: resumeMain4,
+    },
+    {
+        name: "طراحی سایت خبری",
+        thumb: resumeThumb5,
+        main: resumeMain5,
+    },
+    {
+        name: "طراحی سایت شرکتی",
+        thumb: resumeThumb7,
+        main: resumeMain7,
+    },
+    {
+        name: "طراحی سایت و اپلیکیشن شرکتی",
+        thumb: resumeThumb8,
+        main: resumeMain8,
+    },
+    {
+        name: "طراحی سایت شرکتی",
+        thumb: resumeThumb9,
+        main: resumeMain9,
+    },
+    {
+        name: "طراحی سایت کلینیک",
+        thumb: resumeThumb11,
+        main: resumeMain11,
+    },
+    {
+        name: "طراحی سایت شرکتی",
+        thumb: resumeThumb12,
+        main: resumeMain12,
+    },
+    {
+        name: "طراحی سایت مشاوره آنلاین",
+        thumb: resumeThumb13,
+        main: resumeMain13,
+    },
+    {
+        name: "طراحی سایت فروشگاهی",
+        thumb: resumeThumb14,
+        main: resumeMain14,
+    },
+    {
+        name: "طراحی سایت مشاوره آنلاین",
+        thumb: resumeThumb15,
+        main: resumeMain15,
+    },
+    {
+        name: "طراحی سایت فروشگاهی",
+        thumb: resumeThumb16,
+        main: resumeMain16,
+    },
+    {
+        name: "طراحی سایت فروشگاهی",
+        thumb: resumeThumb17,
+        main: resumeMain17,
+    },
+    {
+        name: "طراحی سایت سفارشی",
+        thumb: resumeThumb18,
+        main: resumeMain18,
+    },
+    {
+        name: "طراحی سایت سفارشی",
+        thumb: resumeThumb19,
+        main: resumeMain19,
+    },
+]
+
+export default ResumeList
--- src/views/components/ResumeBox.js
+++ src/views/components/ResumeBox.js
@@ -1,10 +1,11 @@
 import Material from "./Material"
 import React, {useState} from "react"
 import MyLoader from "./MyLoader"
+import {Helmet} from "react-helmet"
 
 function ResumeBox(props)
 {
-    const {resumeThumb, resumeMain} = props
+    const {resumeThumb, resumeMain, name} = props
     const [isShow, setIsShow] = useState(false)
     const [isLoaded, setIsLoaded] = useState(false)
 
@@ -37,11 +38,20 @@ function ResumeBox(props)
         <>
             <Material className="resume-box" onClick={toggleShow}>
                 <img className="resume-box-img" src={resumeThumb} alt=""/>
+                <p className="resume-box-name">{name}</p>
             </Material>
             {
                 isShow &&
                 <div className={`resume-box-img-fix-cont ${isLoaded ? "" : "loading"}`} onClick={toggleShow}>
-                    {isLoaded && <img className="resume-box-img-fix" src={resumeMain} alt=""/>}
+                    {
+                        isLoaded &&
+                        <>
+                            <img className="resume-box-img-fix" src={resumeMain} alt=""/>
+                            <Helmet>
+                                <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes"/>
+                            </Helmet>
+                        </>
+                    }
                     <MyLoader className={`resume-box-img-loading ${isLoaded ? "hide" : ""}`} width={60}/>
                 </div>
             }
